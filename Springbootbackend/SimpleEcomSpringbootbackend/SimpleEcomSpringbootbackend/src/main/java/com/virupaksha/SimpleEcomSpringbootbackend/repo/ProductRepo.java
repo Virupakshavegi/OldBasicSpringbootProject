e37614376802e281d7e8bf4a838dabc70f2fc0f2
Virupakshavegi123 <vegi.virupaksha@bitcoin.com>
2025-02-11 13:39:47 +0530

    first commit

---
+++ Springbootbackend/SimpleEcomSpringbootbackend/SimpleEcomSpringbootbackend/src/main/java/com/virupaksha/SimpleEcomSpringbootbackend/repo/ProductRepo.java
@@ -0,0 +1,20 @@
+package com.virupaksha.SimpleEcomSpringbootbackend.repo;
+
+
+import com.virupaksha.SimpleEcomSpringbootbackend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+@Repository
+public interface ProductRepo  extends JpaRepository<Product,Integer> {
+
+    @Query("SELECT p from Product p WHERE "+
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(String keyword);
+
+}
---
+++ Springbootbackend/SimpleEcomSpringbootbackend/SimpleEcomSpringbootbackend/src/test/java/com/virupaksha/SimpleEcomSpringbootbackend/SimpleEcomSpringbootbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virupaksha.SimpleEcomSpringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEcomSpringbootbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
